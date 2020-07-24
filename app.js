cf13ae38581aa8b131ba7d63c8de037ae8324078
MugishaU <muwiragiye123@gmail.com>
2020-07-24 16:36:26 +0100

    add basic express server to app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const port = 3000;
+const app = express();
+
+app.use(cors());
+app.listen(port, () =>
+  console.log(`Express server running at http://localhost:${port}`)
+);
+
+app.get("/", (req, res) => res.send("app.js server"));
