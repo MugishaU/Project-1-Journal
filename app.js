369ef9bc13eb3fd897fd3b81e6a4a4d7e83b23cc
MugishaU <muwiragiye123@gmail.com>
2020-07-25 22:56:09 +0100

    make example post object in app.js

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const cors = require("cors");
 const port = 3000;
 const app = express();
 
+let posts = [
+  {
+    id: 0,
+    title: "Test Post",
+    description: "A brief post to test with",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus bibendum lectus nec massa eleifend, vitae faucibus odio tincidunt. Pellentesque eleifend, augue nec congue molestie.",
+    gif: "",
+    reaction: {
+      thumbUp: 5,
+      clap: 0,
+      love: 0,
+    },
+    comments: ["comment1", "comment2", "comment3"],
+  },
+];
+
 app.use(cors());
 app.listen(port, () =>
   console.log(`Express server running at http://localhost:${port}`)
