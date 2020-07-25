7340119e3beed70ef19e5f90eb1ca1cbbf4875f3
MugishaU <muwiragiye123@gmail.com>
2020-07-25 23:33:33 +0100

    extract each inividual input from create.html form in create.js

---
+++ frontend/create.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const title = event.target.title.value;
+  const description = event.target.description.value;
+  const content = event.target.content.value;
+
+  submitPost(title, description, content);
+});
+
+function submitPost(title, description, content) {
+  console.log(`Title: ${title}`);
+  console.log(`Description: ${description}`);
+  console.log(`Content: ${content}`);
+}
