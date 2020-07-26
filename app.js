e8e8b603752d65449d05b45861586599f3b6b7cc
MugishaU <muwiragiye123@gmail.com>
2020-07-26 18:30:18 +0100

    make read and write to json file into functions

--- app.js
+++ app.js
@@ -6,25 +6,29 @@ const app = express();
 const port = 3000;
 let posts;
 
+readJSON();
 app.use(cors());
 app.use(bodyParser.text());
 app.listen(port, () =>
   console.log(`Express server running at http://localhost:${port}/posts`)
 );
 
-fs.readFile("./posts.json", "utf8", (err, jsonString) => {
-  if (err) {
-    console.log("Error reading file from disk:", err);
-    return;
-  }
-  try {
-    posts = JSON.parse(jsonString);
-  } catch (err) {
-    posts = { error: `Error parsing JSON string:${err}` };
-  }
-});
+// fs.readFile("./posts.json", "utf8", (err, jsonString) => {
+//   if (err) {
+//     console.log("Error reading file from disk:", err);
+//     return;
+//   }
+//   try {
+//     posts = JSON.parse(jsonString);
+//   } catch (err) {
+//     posts = { error: `Error parsing JSON string:${err}` };
+//   }
+// });
 
-app.get("/posts", (req, res) => res.send(posts));
+app.get("/posts", (req, res) => {
+  readJSON();
+  res.send(posts);
+});
 
 app.post("/posts/newpost", (req, res) => {
   const newPostContent = JSON.parse(req.body);
@@ -46,17 +50,34 @@ app.post("/posts/newpost", (req, res) => {
   newPost.description += newPostContent.description;
   newPost.content += newPostContent.content;
   posts.push(newPost);
-  const jsonString = JSON.stringify(posts);
-  fs.writeFile("./posts.json", jsonString, (err) => {
+  readJSON();
+  writeJSON(posts);
+
+  console.log(newPostContent);
+});
+
+function readJSON() {
+  fs.readFile("./posts.json", "utf8", (err, jsonString) => {
     if (err) {
-      console.log("Error writing file", err);
-    } else {
-      console.log(`Successfully saved "${newPost.title}" to file`);
+      console.log("Error reading file from disk:", err);
+      return;
+    }
+    try {
+      posts = JSON.parse(jsonString);
+    } catch (err) {
+      posts = { error: `Error parsing JSON string:${err}` };
     }
   });
+}
 
-  res.send(console.log(newPostContent));
-});
+function writeJSON(body) {
+  const jsonString = JSON.stringify(body);
+  fs.writeFile("./posts.json", jsonString, (err) => {
+    err
+      ? console.log("Error writing file", err)
+      : console.log("Successfully written to posts.json");
+  });
+}
 
 // {{"id": 0,
 // "title": "Test Post",
