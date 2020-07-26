eae0833ce2b0741b7747fea3211ce8864392d83b
MugishaU <muwiragiye123@gmail.com>
2020-07-26 16:50:22 +0100

    create JSON database to read data from rather than object within app.js

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const port = 3000;
 const bodyParser = require("body-parser");
+const fs = require("fs");
 const app = express();
-
-let idCount = 1;
-let posts = [
-  {
-    id: 0,
-    title: "Test Post",
-    description: "A brief post to test with",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus bibendum lectus nec massa eleifend, vitae faucibus odio tincidunt. Pellentesque eleifend, augue nec congue molestie.",
-    gif: "",
-    reaction: {
-      thumbUp: 5,
-      clap: 0,
-      love: 0,
-    },
-    comments: ["comment1", "comment2", "comment3"],
-  },
-];
+const port = 3000;
+let posts;
 
 app.use(cors());
 app.use(bodyParser.text());
@@ -28,7 +12,19 @@ app.listen(port, () =>
   console.log(`Express server running at http://localhost:${port}/posts`)
 );
 
-app.get("/posts", (req, res) => res.send(posts));
+fs.readFile("./database.json", "utf8", (err, jsonString) => {
+  if (err) {
+    console.log("Error reading file from disk:", err);
+    return;
+  }
+  try {
+    database = JSON.parse(jsonString);
+  } catch (err) {
+    database = { error: `Error parsing JSON string:${err}` };
+  }
+});
+
+app.get("/posts", (req, res) => res.send(database));
 
 app.post("/posts/newpost", (req, res) => {
   const newPostContent = JSON.parse(req.body);
