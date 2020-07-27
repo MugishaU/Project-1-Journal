1aa775b7f016760cf95e78d31c6810eb92d369bd
MugishaU <muwiragiye123@gmail.com>
2020-07-27 14:42:23 +0100

    add barebones track likes functionality

--- frontend/posts.js
+++ frontend/posts.js
@@ -1,45 +1,44 @@
-const test = document.getElementById('test')
-const allPosts =document.getElementById('allPosts')
+const test = document.getElementById("test");
+const allPosts = document.getElementById("allPosts");
 
+fetch("http://localhost:3000/posts")
+  .then((r) => r.json())
+  .then((data) => displayPosts(data));
 
-fetch('http://localhost:3000/posts')
-    .then(r => r.json())
-    .then(data => displayPosts(data))
+function displayPosts(posts) {
+  for (post of posts) {
+    //Title & id//
+    const title = document.createElement("p");
+    const id = document.createElement("p");
+    title.textContent = `Title: ${post.title} `;
+    id.textContent = `Id: #${post.id} `;
+    allPosts.append(title);
+    allPosts.append(id);
+    //Content//
+    const content = document.createElement("p");
+    content.textContent = post.content;
+    allPosts.append(content);
+    //gif//
+    const gif = document.createElement("img");
+    gif.textContent = post;
+    allPosts.append(gif);
+    //reactions//
+    const reaction = document.createElement("button");
+    reaction.textContent = "thumb-up";
+    reaction.setAttribute("href", "#");
+    reaction.setAttribute("id", `thumbUp${post.id}`);
+    allPosts.append(reaction);
 
-
-function displayPosts(posts){
-    for(post of posts){
-        //Title & id//
-        const title = document.createElement('p')
-        const id = document.createElement('p');
-        title.textContent = `Title: ${post.title} `
-        id.textContent = `Id: #${post.id} `
-        allPosts.append(title)
-        allPosts.append(id)
-        //Content//
-        const content = document.createElement('p')
-        content.textContent = post.content;
-        allPosts.append(content)
-        //gif//
-        const gif = document.createElement('img')
-        gif.textContent = post;
-        allPosts.append(gif)
-        //reactions//
-        const reaction = document.createElement('a')
-        reaction.textContent = "thumb-up";
-        reaction.setAttribute("href","#")
-        reaction.setAttribute("id", `thumbUp${post.id}`)
-        allPosts.append(reaction)
-        const reactionCount = document.createElement('p')
-        reactionCount.textContent = `Thumbs-up count: ${post.reaction.thumbUp}`
-        addReaction(`thumbUp${post.id}`, reactionCount, post.reaction.thumbUp);
-        allPosts.append(reactionCount);
-    }
+    const reactionCount = document.createElement("p");
+    reactionCount.textContent = `Thumbs-up count: ${post.reaction.thumbUp}`;
+    allPosts.append(reactionCount);
+  }
 }
 
-function addReaction (reactionID, reactionCounter, currentCount) {
-    
-    reactionID.addEventListener("click", () => {
-        reactionCount.textContent = `Thumbs-up count: ${currentCount += 1}`
-    })
+function add1(button1, place) {
+  document.getElementById(button1).addEventListener("click", () => {
+    console.log("click");
+  });
 }
+
+add1("thumbUp1");
